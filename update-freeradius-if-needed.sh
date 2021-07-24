1f2d90e1df8844643b4f8cae5e564dddda5a1786
Yo <yoanm@users.noreply.github.com>
2021-07-24 11:16:43 +0200

    Create update-freeradius-if-needed.sh

---
+++ update-freeradius-if-needed.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+# Available vars 
+# $RENEWED_DOMAINS (Ex: "test.domain.tld" or "test.domain.tld test2.domain.tld ...")
+# $RENEWED_LINEAGE (Ex: "/etc/letsencrypt/live/test.domain.tld"
+#
+# Script will move private and public key to freeradius config directory and restart it
+
+TARGET_DOMAIN='XXXXXX.YYYYYY.ZZZZZ'
+FREERADIUS_CERTIFICATE_FOLDER='/etc/freeradius/3.0/certs'
+
+RESTART_REQUIRED=0
+for DOMAIN in $RENEWED_DOMAINS; do
+    case $DOMAIN in
+        $TARGET_DOMAIN)
+            RESTART_REQUIRED=1
+        ;;
+    esac
+done
+
+if [ $RESTART_REQUIRED -eq 1 ]; then
+    echo '#### [INFO] Updating freeradius certificates'
+    cp ${RENEWED_LINEAGE}/{fullchain,privkey}.pem ${FREERADIUS_CERTIFICATE_FOLDER}/
+    chown freerad:freerad ${FREERADIUS_CERTIFICATE_FOLDER}/{fullchain,privkey}.pem
+    echo '#### [INFO] Restarting freeradius'
+    /etc/init.d/freeradius restart
+    echo '#### [INFO] freeradius updated & restarted'
+fi
